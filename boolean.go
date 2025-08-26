2e75f22b7b42b76b5a593be8486c5dd0c9d8b729
xiapeng <sherlockhusky@qq.com>
2025-08-26 18:43:46 +0800

    string basic

---
+++ boolean.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	bool1 := true
+	bool2 := false
+	if bool1 && bool2 {
+		fmt.Printf("The value is true\n")
+	} else {
+		fmt.Printf("The value is false\n")
+	}
+}
